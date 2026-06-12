74d592fe678a100d62cec54c1eb558fb6e7779c7
Edgar Beck <beck@ant.uni-bremen.de>
2026-06-12 14:41:10 +0200

    Moved setting deterministic seed

--- gpu_select_tf.py
+++ gpu_select_tf.py
@@ -8,6 +8,7 @@ Created on Tue Jan 22 16:49:30 2019
 
 
 import tensorflow as tf
+import os
 
 
 def gpu_select(number=0, memory_growth=True, cpus=0):
@@ -45,5 +46,37 @@ def gpu_select(number=0, memory_growth=True, cpus=0):
     else:
         print('Will choose GPU or CPU automatically.')
 
+
+# def set_deterministic(seed=42):
+#     """
+#     Forces deterministic behavior across different machines.
+
+#     Args:
+#         seed: Random seed to use
+#     """
+#     import random
+#     import platform
+#     # Seeds
+#     os.environ['PYTHONHASHSEED'] = str(seed)
+#     random.seed(seed)
+#     np.random.seed(seed)
+#     tf.random.set_seed(seed)
+
+#     # Deterministische TF Operationen
+#     os.environ['TF_DETERMINISTIC_OPS'] = '1'
+#     os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+
+#     # oneDNN deaktivieren
+#     os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+#     # Threads fixieren
+#     tf.config.threading.set_inter_op_parallelism_threads(1)
+#     tf.config.threading.set_intra_op_parallelism_threads(1)
+
+#     print(f"Python:     {sys.version}")
+#     print(f"TensorFlow: {tf.__version__}")
+#     print(f"Keras:      {keras.__version__}")
+#     print(f"NumPy:      {np.__version__}")
+#     print(f"CPU:        {platform.processor()}")
 
 # EOF
